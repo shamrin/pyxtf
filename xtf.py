e19f303e6f3ef5e1d0aa09e879ad28387d942956
Alexey Shamrin <shamrin@gmail.com>
2013-03-12 20:04:10 +0400

    move [un]wrap to `sacker` module

--- xtf.py
+++ xtf.py
@@ -5,13 +5,14 @@ From command line (requires Matplotlib):
 """
 
 import sys
-import re
-from struct import Struct
 from pprint import pprint, pformat
 from collections import OrderedDict, namedtuple
 from itertools import groupby, islice
 
 import numpy as np
+
+from sacker import wrap, unwrap, BadDataError
+import segy
 
 CHAN_TYPES = {
     0: 'subbottom',
@@ -345,85 +346,6 @@ def packets_gen(data, chaninfos, packet_filter):
         i += 1
 
     sys.stdout.write('\n')
-
-class BadDataError(Exception):
-    pass
-
-def unwrap(binary, spec, data_name=None, dict_factory=dict):
-    """Unwrap `binary` according to `spec`, return (consumed_length, data)
-
-    Basically it's a convenient wrapper around struct.unpack. Each non-empty
-    line in spec must be: <struct format> <field name> [== <test> <action>]
-
-    struct format - struct module format producing exactly one value
-    field name - dictionary key to put unpacked value into
-    test - optional test an unpacked value be equal to
-    action - what to do if test failed: `!` (bad data) or `?` (unsupported)
-
-    Example:
-    >>> unwrap('\x0a\x00DATA\x00something else', '''h magic == 0x0a !
-    ...                                             4s data''')
-    (6, {'magic': 10, 'data': 'DATA'})
-    """
-
-    struct, names, tests, s_indices = parse(spec)
-
-    # unpack binary data
-    length = struct.size
-    sub = binary[:length]
-    if isinstance(sub, memoryview):
-        sub = sub.tobytes()
-    values = list(struct.unpack(sub))
-
-    # strip padding from end of strings
-    for i in s_indices:
-        values[i] = values[i].rstrip('\x00')
-
-    # run optional tests
-    for i, test, action in tests:
-        if values[i] != test:
-            adj = {'!': 'Bad', '?': 'Unsupported'}[action]
-            raise BadDataError(' '.join(w for w in
-                    [adj, data_name, names[i], '== %r' % values[i]] if w))
-
-    return length, dict_factory(zip(names, values))
-
-def wrap(data, spec):
-    struct, names, tests, s_indices = parse(spec)
-    return struct.pack(*[data[name] for name in names])
-
-_cache = {}
-def parse(spec):
-    try:
-        return _cache[spec]
-    except KeyError:
-        matches = [re.match("""(\w+)           # struct format
-                               \s+
-                               (\w+)           # field name
-                               \s*
-                               (==\s*(.+)\ ([!?]))? # optional test-action
-                               $""", s.strip(), re.VERBOSE)
-                   for s in spec.split('\n') if s and not s.isspace()]
-
-        for n, m in enumerate(matches):
-            if not m: raise SyntaxError('Bad unwrap spec, LINE %d' % (n+1))
-
-        formats = [m.group(1) for m in matches]
-        names = [m.group(2) for m in matches]
-
-        tests = [(m.group(4), m.group(5)) for m in matches]
-        tests = [(i, eval(test, {}, globals()), action)
-                 for i, (test, action) in enumerate(tests) if test]
-
-        # string format spec indices
-        s_indices = [i for i, c in enumerate(formats)
-                       if re.match(r'(\d+)s', c)]
-
-        struct = Struct('<' + ''.join(formats))
-
-        _cache[spec] = struct, names, tests, s_indices
-        return _cache[spec]
-
 
 def read_XTF_as_grayscale_arrays(infile):
     header, chaninfos, packets = read_XTF(infile, 'sonar')
