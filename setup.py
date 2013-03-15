7e550bc37b0b71bc996fe1f63e896879f7a7e67b
Alexey Shamrin <shamrin@gmail.com>
2013-03-15 23:57:26 +0400

    docs: build to html, link from start menu

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from distutils.core import setup
+from subprocess import call
+
 import GUI.py2exe
 import py2exe
 
 execfile('version.py')
 
+print 'Building docs via GitHub API...'
+call(["c:/Python27/python" , "doc/build_docs", "doc/README_ru.markdown",
+                                               "doc/README_ru.html"])
+
 setup(console = ['xtfgui.py'],
       #windows = [ {'script': 'xtfgui.py', 'version': __version__ }],
-      options = { 'py2exe': { 'excludes': ['Tkinter', '_ssl'] }})
+      options = { 'py2exe': { 'excludes': ['Tkinter', '_ssl'] }},
+      data_files = [('.', ['doc/README_ru.html', 'doc/screenshot.png'])])
